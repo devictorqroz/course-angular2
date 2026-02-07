011ad6357477d35ea6a33125e58521af17ac5ab1
Victor Queiroz <victorqueiroz.ti@gmail.com>
2026-02-07 11:59:52 -0300

    feat: implement event binding and event filtering

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -3,7 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css']
+  // styleUrls: ['./data-binding.component.css']
+  styles: [
+    `
+      .highlight {
+        background-color: yellow;
+        font-weight: bold;
+      }
+    `
+  ]
 })
 export class DataBindingComponent implements OnInit {
 
@@ -11,12 +19,34 @@ export class DataBindingComponent implements OnInit {
   angularCourse: boolean = true;
   imageUrl: string = 'https://picsum.photos/400/200';
 
+  currentKeyEntered: string = '';
+  savedValue: any;
+
+  isMouseOver: boolean = false;
+
   getValue() {
     return 1;
   }
 
   isLikedCourse() {
     return true;
+  }
+
+  onButtonClick() {
+    alert('The Button was clicked!');
+  }
+
+  onKeyUp(event: KeyboardEvent) {
+    this.currentKeyEntered = (<HTMLInputElement>event.target).value;
+    console.log(this.currentKeyEntered);
+  }
+
+  saveValue(value) {
+    this.savedValue = value;
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
   }
 
   constructor() { }
