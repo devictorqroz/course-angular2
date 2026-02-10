4456832d009e578360f1499c9a7d87bb6ff8e9d0
Victor Queiroz <victorqueiroz.ti@gmail.com>
2026-02-10 09:10:11 -0300

    feat: config SCSS as default style and test nesting in example component

--- directives/src/app/app.module.ts
+++ directives/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { DirectiveNgifComponent } from './directive-ngif/directive-ngif.component';
+import { ExampleComponent } from './example/example.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DirectiveNgifComponent
+    DirectiveNgifComponent,
+    ExampleComponent
   ],
   imports: [
     BrowserModule,
