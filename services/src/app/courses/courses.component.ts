6a0bb5ed70482467fb093b69621af4e8554aa1c0
Victor Queiroz <victorqueiroz.ti@gmail.com>
2026-02-27 21:30:58 -0300

    feat: implement manual service instantiation in CoursesComponent

---
+++ services/src/app/courses/courses.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+import { CoursesService } from './courses.service';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.css']
+})
+export class CoursesComponent implements OnInit {
+
+  courses: string[] = [];
+
+  coursesService: CoursesService;
+
+  constructor() {
+    this.coursesService = new CoursesService();
+  }
+
+  ngOnInit() {
+    this.courses = this.coursesService.getCourses();
+  }
+
+}
