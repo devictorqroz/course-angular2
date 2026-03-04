a370944cb1030b6b1ce03ae12882ec0c7a92f82b
Victor Queiroz <victorqueiroz.ti@gmail.com>
2026-03-04 17:55:40 -0300

    feat: implement basic routing with Home, Login and Courses components using RouterModule

--- study-routes/src/app/app.module.ts
+++ study-routes/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CoursesComponent } from './courses/courses.component';
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
